702a538a95662b23ba32e8db38b408429575ab4f
Freddie Liardet <frederick.liardet@arm.com>
2025-09-01 15:35:48 +0100

    Add pip packaging support
    Change-Id: I617e0dd78623f73176d6b158ca5bb4f14e0c1771
    Signed-off-by: Freddie Liardet <frederick.liardet@arm.com>

---
+++ pip_package/setup.py
@@ -0,0 +1,24 @@
+#
+# SPDX-FileCopyrightText: Copyright 2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+# SPDX-License-Identifier: Apache-2.0
+#
+from datetime import datetime
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="ai_ml_sdk_vgf_lib",
+    version=datetime.today().strftime("%m.%d"),
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "vgf_lib": ["binaries/*", "binaries/*/*", "binaries/*/*/*"],
+    },
+    entry_points={
+        "console_scripts": [
+            "vgf_dump=vgf_lib.cli:main",
+        ]
+    },
+    zip_safe=False,
+)
