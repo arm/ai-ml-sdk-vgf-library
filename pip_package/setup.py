8625db1a871d7caf305ddb26367c83fb12fbf78f
Alan Liang <alan.liang@arm.com>
2025-10-02 13:06:51 +0100

    Align package names with repo names
    Signed-off-by: Alan Liang <alan.liang@arm.com>
    Change-Id: I65ee0ec31d468aa315f5587e8bf524e1ff2d820d

--- pip_package/setup.py
+++ pip_package/setup.py
@@ -8,7 +8,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name="ai_ml_sdk_vgf_lib",
+    name="ai_ml_sdk_vgf_library",
     version=datetime.today().strftime("%m.%d"),
     packages=find_packages(),
     include_package_data=True,
