edbffdb11c687b24115212fcd2866cc994524165
Awadhy Mohammed <89090572+AwadhyM@users.noreply.github.com>
2025-12-02 11:23:40 +0000

    Fix uncaught exceptions in vgf_converter main (#86)
    This change ensures that argparse and filesystem validation are
    wrapped in a try-catch block. This prevents any uncaught exceptions.
    
    Change-Id: I28b11a19986b1ce1cf27d6497eadb9037a281aa0
    
    Signed-off-by: Awadhy Mohammed <awadhy.mohammed@arm.com>

--- vgf_converter/src/main.cpp
+++ vgf_converter/src/main.cpp
@@ -10,37 +10,37 @@
 #include <stdexcept>
 
 int main(int argc, char *argv[]) {
-    argparse::ArgumentParser parser(argv[0]);
-
-    // Set up positional args
-    parser.add_argument("-i", "--input").help("The VGF input file to convert").required();
-    parser.add_argument("-o", "--output").help("Path to updated VGF file").required();
-
-    parser.parse_args(argc, argv);
-    const std::string input = parser.get("--input");
-    const std::string output = parser.get("--output");
-
-    // Validate paths
-    const std::filesystem::path inputPath{input};
-    const std::filesystem::path outputPath{output};
-
-    if (!std::filesystem::exists(inputPath)) {
-        std::cerr << "File does not exist: " << input << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    if (!std::filesystem::is_regular_file(inputPath)) {
-        std::cerr << "Input path is not a file: " << input << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    // Check that input and output paths are different
-    if (std::filesystem::exists(outputPath) && std::filesystem::equivalent(inputPath, outputPath)) {
-        std::cerr << "Input path '" << input << "' is identical to output path '" << output << "'" << std::endl;
-        return EXIT_FAILURE;
-    }
-
     try {
+        argparse::ArgumentParser parser(argv[0]);
+
+        // Set up positional args
+        parser.add_argument("-i", "--input").help("The VGF input file to convert").required();
+        parser.add_argument("-o", "--output").help("Path to updated VGF file").required();
+
+        parser.parse_args(argc, argv);
+        const std::string input = parser.get("--input");
+        const std::string output = parser.get("--output");
+
+        // Validate paths
+        const std::filesystem::path inputPath{input};
+        const std::filesystem::path outputPath{output};
+
+        if (!std::filesystem::exists(inputPath)) {
+            std::cerr << "File does not exist: " << input << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        if (!std::filesystem::is_regular_file(inputPath)) {
+            std::cerr << "Input path is not a file: " << input << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        // Check that input and output paths are different
+        if (std::filesystem::exists(outputPath) && std::filesystem::equivalent(inputPath, outputPath)) {
+            std::cerr << "Input path '" << input << "' is identical to output path '" << output << "'" << std::endl;
+            return EXIT_FAILURE;
+        }
+
         mlsdk::vgf_converter::convert(input, output);
     } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
