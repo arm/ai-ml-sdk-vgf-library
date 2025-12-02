a5be7bcd0d6b7588e4cc898975972fa8e6885f5c
Alan Liang <alan.liang@arm.com>
2025-12-02 14:40:04 +0000

    Return status code in pip package
    Signed-off-by: Alan Liang <alan.liang@arm.com>
    Change-Id: I33e3d58ed36d0013b3b2f2e0c249a3b366f6ca35

--- pip_package/vgf_lib/cli.py
+++ pip_package/vgf_lib/cli.py
@@ -10,4 +10,4 @@ import sys
 def main():
     binary_name = "vgf_dump.exe" if sys.platform.startswith("win") else "vgf_dump"
     binary_path = os.path.join(os.path.dirname(__file__), "binaries/bin", binary_name)
-    subprocess.run([binary_path] + sys.argv[1:])
+    return subprocess.run([binary_path] + sys.argv[1:])
