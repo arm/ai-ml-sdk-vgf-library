a3690d9095cfce53f84e28cbdfd6649d064d2f15
Alan Liang <alan.liang@arm.com>
2025-12-03 09:25:27 +0000

    Fix return code in pip package
    Signed-off-by: Alan Liang <alan.liang@arm.com>
    Change-Id: Ifab4c6f07fc54cabcda870c8764ff9a379698236

--- pip_package/vgf_lib/cli.py
+++ pip_package/vgf_lib/cli.py
@@ -10,4 +10,5 @@ import sys
 def main():
     binary_name = "vgf_dump.exe" if sys.platform.startswith("win") else "vgf_dump"
     binary_path = os.path.join(os.path.dirname(__file__), "binaries/bin", binary_name)
-    return subprocess.run([binary_path] + sys.argv[1:])
+    result = subprocess.run([binary_path] + sys.argv[1:])
+    return result.returncode
