12e04f27f795b3e6f7bd0ea8c75a79382bb5a1b8
Mohammadreza Ameri Mahabadian <mohammadreza.amerimahabadian@arm.com>
2025-12-23 13:36:02 +0000

    Update pip-package platform tagging (#98)
    Change-Id: Ic90f312c605cb914ed459b41a96bdece43edb174
    
    Signed-off-by: Mohammadreza Ameri Mahabadian <mohammadreza.amerimahabadian@arm.com>

--- pip_package/setup.py
+++ pip_package/setup.py
@@ -2,6 +2,8 @@
 # SPDX-FileCopyrightText: Copyright 2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
 # SPDX-License-Identifier: Apache-2.0
 #
+import platform
+
 from setuptools import setup
 from wheel.bdist_wheel import bdist_wheel
 
@@ -12,7 +14,20 @@ class BDistWheel(bdist_wheel):
         self.root_is_pure = False
 
     def get_tag(self):
-        platformName = super().get_tag()[2]
+        system = platform.system()
+        machine = platform.machine()
+        if system == "Windows":
+            assert machine == "x86_64"
+            platformName = "win_amd64"
+        elif system == "Linux":
+            if machine == "aarch64":
+                platformName = "manyLinux2014_aarch64"
+            else:
+                assert machine == "x86_64"
+                platformName = "manyLinux2014_x86_64"
+        elif system == "Darwin":
+            assert machine == "arm64"
+            platformName = "macosx_10_13_arm64"
         return ("py3", "none", platformName)
 
 
